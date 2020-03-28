bb3336e18760ec31ea775a067db12273bbf390cf
AbhirajPatel <abhirajsinghpatel@gmail.com>
2020-03-29 01:30:03 +0530

    initial spring-boot setup with db configurations

---
+++ src/test/java/com/kisan/adduser/AddUserApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kisan.adduser;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AddUserApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
